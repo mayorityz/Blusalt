12c980f74aa02216da5a425d285ff2ce85ea9507
Mayowa <mayority11@gmail.com>
2022-12-07 04:19:19 +0100

    fixes

--- services/customer/server.ts
+++ services/customer/server.ts
@@ -2,16 +2,43 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import initDb from "./DB";
 import CustomerRoutes from "./customer.route"
-
+import {connect} from "amqplib"
+import {customerModel} from "./customer.model"
 
 dotenv.config();
 initDb();
 
+var channel, connection;
+
 const app: Express = express();
 const port = process.env.PORT || 4564;
+const amqpServer = process.env.RabbitMqUrl || "amqp://localhost:5672"
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
+
+
+connectQueue() // call connectQueue function
+async function connectQueue() {
+    try {
+        connection = await connect(amqpServer);
+        channel = await connection.createChannel()
+        
+        await channel.assertQueue("updateRecord")
+        channel.consume("updateRecord", data => {
+            console.log("Data received : ", `${Buffer.from(data.content)}` );
+            let _data = JSON.parse(data.content)
+            channel.ack(data)
+            // update the db
+            // customerModel.updateOne({_id : _data._id}, {})
+            console.log("logged : ")
+            console.log(data.content)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 app.get("/", (req: Request, res: Response) => {
   console.log(req.body);
